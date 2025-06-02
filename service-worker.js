f9adbbd3fe65604875195fb94f9c20634e22ce63
taa66since98 <pllololens@gmail.com>
2025-06-03 00:36:48 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Service Worker for offline functionality
+const CACHE_NAME = 'korean-learning-v1';
+const urlsToCache = [
+  '/Foraforeigner/',
+  '/Foraforeigner/index.html',
+  '/Foraforeigner/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});

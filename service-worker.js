62a1c2034b3302944fcaecfa4a8ce278530e68ad
taa66since98 <pllololens@gmail.com>
2025-06-03 22:16:55 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,27 @@
-// Service Worker for offline functionality
 const CACHE_NAME = 'korean-learning-v1';
 const urlsToCache = [
-  '/Foraforeigner/',
-  '/Foraforeigner/index.html',
-  '/Foraforeigner/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch event - serve from cache when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Activate event - clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('./index.html');
+      })
   );
 });
